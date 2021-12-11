0ccf635cab3827e83c4b591a35537fa0b8352b3b
w1843008835 <chenyw1843@gmail.com>
2021-12-11 16:11:05 +0900

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('hello_world',blog.views.hello_world),
     path('content',blog.views.article_content),
     path('index', blog.views.get_index_page),
-    path('detail', blog.views.get_detail_page)
+    # path('detail', blog.views.get_detail_page)
+    path('detail/<str:article_id>', blog.views.get_detail_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,32 @@ def get_index_page(request):
     all_article = Articles.objects.all()
     return render(request,'blog/index.html',{'article_list':all_article})
 
-def get_detail_page(request):
-    cur_article = Articles.objects.all()[0]
+def get_detail_page(request,article_id):
+    all_article = Articles.objects.all()
+    cur_article = None
+    pre_article = None
+    next_article = None
+    pre_index = 0
+    next_index = 0
+    for index,articles in enumerate(all_article):
+        if int(article_id) == int(articles.article_id):
+            if index == 0:
+                pre_index = 0
+                next_index = int(index) + 1
+            elif index == len(all_article) - 1:
+                next_index = int(index)
+                pre_index = int(index) - 1
+            else:
+                pre_index = int(index) - 1
+                next_index = int(index) + 1
+
+            cur_article = articles
+            pre_article = all_article[pre_index]
+            next_article = all_article[next_index]
+            break
     section_list = cur_article.content.split("\n")
-    return render(request,'blog/detail.html',{'cur_article':cur_article,'section_list':section_list})
+    return render(request,'blog/detail.html',{'cur_article':cur_article,
+                                              'section_list':section_list,
+                                              'pre_article':pre_article,
+                                              'next_article':next_article
+                                              })
